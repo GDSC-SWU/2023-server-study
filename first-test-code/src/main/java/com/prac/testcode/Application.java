22af15ea3cf5925047f95e8207ff1c565ff58b4f
Koominji <mjk2715@naver.com>
2023-09-28 20:57:53 +0900

    feat: add 2nd week code

---
+++ first-test-code/src/main/java/com/prac/testcode/Application.java
@@ -0,0 +1,13 @@
+package com.prac.testcode;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing // jpa auditing 활성화
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args){
+        SpringApplication.run(Application.class,args);
+    }
+}
--- shopping_mall_project/src/main/java/com/project/shopping/domain/products/ProductsRepository.java
+++
@@ -1,8 +0,0 @@
-package com.project.shopping.domain.products;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductsRepository extends JpaRepository<Products,Long> {
-
-
-}
